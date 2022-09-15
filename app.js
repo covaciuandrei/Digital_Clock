c977dd1c35fe6430a1b4d0cd77e3bd8adfda7a74
covaciuandrei <81052535+covaciuandrei@users.noreply.github.com>
2022-09-15 16:09:35 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const clock = document.querySelector('.clock');
 
 const tick = () => {
-    
+
     const now = new Date();
 
     const h = now.getHours();
     const m = now.getMinutes();
     const s = now.getSeconds();
-    
-    const html = `
-    <span>${h}</span> :
-    <span>${m}</span> :
-    <span>${s}</span>
-    `;
+    let sec ='';
+    let min ='';
+    let hh = '';
 
+
+    if (s < 10){
+        sec=`0`;
+    }
+    if (m < 10){
+        min=`0`;
+    }
+    if (h<10){
+        hh=`0`;
+    }
+
+    console.log(sec, min, hh);
+    let html = `
+    <span>${hh}${h}</span> :
+    <span>${min}${m}</span> :
+    <span>${sec}${s}</span>
+    `;
     clock.innerHTML = html;
 
 };
